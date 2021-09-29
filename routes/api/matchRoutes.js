3f191cc3784909fb604359f4b592a5d590e8b3f7
Jonathan Bokungu <jonathanbokungu21@gmail.com>
2021-09-29 13:47:00 -0400

    updated routes

--- routes/api/matchRoutes.js
+++ routes/api/matchRoutes.js
@@ -1,1 +1,6 @@
 const router = require('express').Router();
+const Match = require('../../models/Match');
+
+router.get('/', async (req, res) => {
+    
+})
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
 
-const routes = require('./controllers');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -19,6 +19,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
+
