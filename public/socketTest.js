ec16fabcebefc846352feabfe85a40f02296cdb9
LilyWS <stranger1212ss23@gmail.com>
2021-09-29 13:38:01 -0400

    finished battle prototype and modularized socket

--- public/socketTest.js
+++ public/socketTest.js
@@ -1,10 +1,14 @@
 var socket = io();
 
-let input = document.getElementById("player1");
+const input = document.getElementById("player1");
+const p2Display = document.getElementById('player2')
+var previousInput = "";
+var p2String = "";
 
-input.addEventListener("keydown",(e) =>{
-  console.log(e.code)
-  if (e.code) {
-    socket.emit('type', e.code);
-  }
+input.addEventListener("keyup",(e) =>{
+    socket.emit('type', input.value);
+});
+
+socket.on('p2typed', (data) => {
+    p2Display.value = data;
 });
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 const path = require('path');
 const express = require('express');
-//const exphbs = require('express-handlebars');
-const socket = require('socket.io');
-const http = require('http');
+const exphbs = require('express-handlebars');
+//const socket = require('socket.io');
 
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const { Match, Text } = require('./models');
 
 const app = express();
-const server = http.createServer(app)
-const io = socket(server);
+const server = require('http').createServer(app)
+//const io = socket(server);
 const PORT = process.env.PORT || 3001;
-
+require('./socket')(server);
 
 const hbs = exphbs.create({});
 
@@ -25,23 +24,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-/*const testMatch = async () => {
-    try{
-        const allMatchData = await Match.findAll({
-            include: [{ model: Text }],
-        });
-        console.log();
-    } catch (err) {
-        console.log(err);
-    }
-}*/
-
-io.on('connection', (socket) => {
-    console.log('got connection');
-    socket.on('type', (data) => {
-        socket.broadcast('p2typed');
-    });
-});
+// io.on('connection', (socket) => {
+//     console.log('got connection');
+//     socket.on('type', (data) => {
+//         console.log(data)
+//         socket.broadcast.emit('p2typed', data);
+//     });
+// });
 
 sequelize.sync({ force: false }).then(() => {
     server.listen(PORT, () => console.log('Now listening'));
