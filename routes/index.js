8927b5b583478737b7264f42da59ff74f2b9e929
Jonathan Bokungu <jonathanbokungu21@gmail.com>
2021-09-30 13:34:38 -0400

    updated seed file

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const apiRoutes = require('./api');
 const testRoutes = require('./test')
 const path = require('path');
 
-//router.use('/api', apiRoutes);
+router.use('/api', apiRoutes);
 
 //the following are testing routes. just let me know if you wanna delete them -lily.
 router.use('/test', testRoutes);
