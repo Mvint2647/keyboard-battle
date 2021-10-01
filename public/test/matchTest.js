886c2d3056b2e5e662bad453ffafcbe1aef7d301
LilyWS <stranger1212ss23@gmail.com>
2021-10-01 14:58:46 -0400

    added gameplay

--- public/test/matchTest.js
+++ public/test/matchTest.js
@@ -1,19 +1,46 @@
 const socket = io();
 
 const input = document.getElementById("player1");
-const p2Display = document.getElementById('player2')
-const loginBtn = document.getElementById('login');
+const p2TextDisplay = document.getElementById('player2');
+const textDisplayEl = document.getElementById('gameStateText');
+const myScoreDisplay = document.getElementById('myScore');
+const p2ScoreDisplay = document.getElementById('p2Score');
 const matchID = window.location.href.substring(window.location.href.lastIndexOf("/")+1, window.location.href.length);
-
+var myPID = null; //tells them which score is theirs
+var p2PID;
 var previousInput = "";
 var p2String = "";
 
-socket.emit('matchJoin', matchID);
 
-input.addEventListener("keyup",(e) =>{
+
+socket.on('gameStart', (word) => {
+    textDisplayEl.textContent = word;
+    input.removeAttribute('disabled');
+});
+
+socket.on('matchCreated', (pid) => {
+    myPID = pid;
+    p2PID = (pid == 1) ? 0 : 1;
+    socket.emit('ready', matchID);
+});
+
+socket.on('newWord', ({word, score}) => {
+    textDisplayEl.textContent = word;
+    myScoreDisplay.textContent = `P1 Score: ${score[myPID]}`;
+    p2ScoreDisplay.textContent = `P2 Score: ${score[p2PID]}`;
+    input.value = "";
+    p2TextDisplay.value = "";
+});
+
+window.addEventListener("load", () => {
+    socket.emit('matchJoin', matchID);//wait before sending otherwise this client wont be ready to recieve messages  
+});
+
+input.addEventListener('keyup',(e) =>{
     socket.emit('type', { text: input.value, queryID: matchID });
 });
 
 socket.on('p2typed', (data) => {
-    p2Display.value = data;
+    p2TextDisplay.value = data;
 });
+
--- utils/index.js
+++ utils/index.js
@@ -1,5 +1,6 @@
-const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 const charactersLength = characters.length;
+const randomWords = require('random-words');
 
 module.exports = {
     createQueryID: (len = 5) => {
@@ -8,5 +9,11 @@ module.exports = {
             result += characters.charAt(Math.floor(Math.random() * charactersLength));
         }
         return result;
+    },
+    getRandomWord: (length) => {
+        return randomWords()
+    },
+    toNormalCase: (text) => {
+        return text.charAt(0).toUpperCase() + text.slice(1);
     }
 }
