196a7802741a9bd95c045101fcfdce4325bbd646
Mel Vint <melvint@Mels-MacBook-Pro.local>
2021-09-30 23:25:44 -0400

    added loginstyle

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,24 @@ const path = require('path');
 
 //router.use('/api', apiRoutes);
 
-//the following are testing routes. just let me know if you wanna delete them -lily.
-router.use('/test', testRoutes);
+// //the following are testing routes. just let me know if you wanna delete them -lily.
+// router.use('/test', testRoutes);
 
-router.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/test/homepageSOCKETTEST.html'));
-});
+// router.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../public/test/homepageSOCKETTEST.html'));
+// });
+
+router.get('/match',(req, res) => {
+    res.render('matchpage')
+})
+
+router.get('/login',(req, res) => {
+    res.render('login')
+})
+
+router.get('/',(req, res) => {
+    res.render('homepage')
+})
+
 
 module.exports = router;
