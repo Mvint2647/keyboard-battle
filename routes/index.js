6915e994803ac29a6f060e4aaa29b2c24ce43da8
LilyWS <stranger1212ss23@gmail.com>
2021-10-04 10:55:18 -0400

    adding production routes

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,18 @@ const path = require('path');
 
 router.use('/api', apiRoutes);
 
+<<<<<<< HEAD
 //the following are testing routes. just let me know if you wanna delete them -lily.
 router.use('/test', testRoutes);
 
 // router.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../public/test/homepageSOCKETTEST.html'));
 // });
+=======
+router.get('/', (req, res) => {
+    res.render('homepage')
+});
+>>>>>>> 7bd578b (adding production routes)
 
 router.get('/match',(req, res) => {
     res.render('matchpage')
@@ -22,5 +28,8 @@ router.get('/',(req, res) => {
 router.get('/login',(req, res) => {
     res.render('login')
 })
+
+//the following are testing routes. just let me know if you wanna delete them -lily.
+router.use('/test', testRoutes);
 
 module.exports = router;
--- routes/test/index.js
+++ routes/test/index.js
@@ -3,8 +3,12 @@ const path = require('path');
 const { createQueryID } = require('../../utils');
 const { Match } = require('../../models');
 
-router.post('/createuser', async (req, res) => {
+// router.post('/createuser', async (req, res) => {
 
+// });
+
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/test/homepageSOCKETTEST.html'));
 });
 
 router.post('/login', async (req, res) => {
