d3a6dff51b71c86510e386fdb768e7b99ea22529
LilyWS <stranger1212ss23@gmail.com>
2021-10-04 11:44:43 -0400

    creating match routes

--- routes/api/matchRoutes.js
+++ routes/api/matchRoutes.js
@@ -1,8 +1,19 @@
 const router = require('express').Router();
 const Match = require('../../models/Match');
+const { createQueryID } = require('../../utils');
 
-router.get('/', async (req, res) => {
-    
-})
+router.get('/create', async (req, res) => {
+    if(req.session.loggedIn) {
+        const newMatch = await Match.create({
+            queryID: createQueryID(),
+            player1_id: req.session.playerID
+        });
+        res.redirect(`../../match/${newMatch.queryID}`);
+    } else {
+        res.redirect('../../../');
+    }
+});
+
+router.get()
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,14 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const testRoutes = require('./test')
 const path = require('path');
+const {Match} = require('../models');
 
 router.use('/api', apiRoutes);
-
-<<<<<<< HEAD
-//the following are testing routes. just let me know if you wanna delete them -lily.
-router.use('/test', testRoutes);
 
 // router.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, '../public/test/homepageSOCKETTEST.html'));
 // });
-=======
-router.get('/', (req, res) => {
-    res.render('homepage')
-});
->>>>>>> 7bd578b (adding production routes)
 
-router.get('/match',(req, res) => {
-    res.render('matchpage')
-})
 router.get('/',(req, res) => {
     res.render('homepage')
 
@@ -28,6 +17,25 @@ router.get('/',(req, res) => {
 router.get('/login',(req, res) => {
     res.render('login')
 })
+
+//match directories
+router.get('/match',(req, res) => {
+    res.render('matchpage')
+})
+
+router.get('/match/:matchID', async (req, res) => {
+    if(req.session.loggedIn) {
+        const matchID = req.params.matchID;
+        const requestedMatch = await Match.findAll({
+            where: {
+                queryID: matchID
+            }
+        })
+        res.render('matchpage');
+    } else {
+        res.redirect('../../');
+    }
+});
 
 //the following are testing routes. just let me know if you wanna delete them -lily.
 router.use('/test', testRoutes);
